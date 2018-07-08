4991d43333317641e30509c3587a559a080a9524
cmy <1018617499@qq.com>
2018-07-08 10:02:15 +0800

    第三次提交

--- src/main/java/com/cmy/App.java
+++ src/main/java/com/cmy/App.java
@@ -2,13 +2,16 @@ package com.cmy;
 
 import static java.lang.Integer.*;
 /**
- * Hello world!
+ * 分支:保证多个人开发一个工程的前提
+ * master:主分支
+ *
+ * 创建分支:
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("HelloWorld");
+        System.out.println("CMY");
     }
 }
---
+++ src/main/java/com/cmy/CmyTest.java
@@ -0,0 +1,8 @@
+package com.cmy;
+
+/**
+ * Created by cmy on 2018/7/8.
+ */
+public class CmyTest {
+    //cmy分支创建的文件
+}
