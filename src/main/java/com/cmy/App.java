bf39d37d97fbfe1170971237c4584cb44a51d3e1
cmy <1018617499@qq.com>
2018-07-08 09:49:12 +0800

    IdeaTest

---
+++ src/main/java/com/cmy/App.java
@@ -0,0 +1,14 @@
+package com.cmy;
+
+import static java.lang.Integer.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(parseInt("123"));
+    }
+}
